ae5dff437853876e28277d6f0981179a03848ab6
RajithaKumara <tmrkumara.15@cse.mrt.ac.lk>
2018-10-25 01:44:46 +0530

    Update Configuration.properties settings

--- build/travis/before_script.sh
+++ build/travis/before_script.sh
@@ -22,7 +22,7 @@ echo -e "\n127.0.0.1    php56 php70 php71 php72" | sudo tee -a /etc/hosts
 cat /etc/hosts
 
 # Edit Configuration.properties
-expression="s~driverPath.*~driverPath=/usr/bin/chromedriver~g; s~pathToInstaller.*~pathToInstaller=http://php56/~g; s~databasePassword.*~databasePassword=root~g; s~databaseHostName.*~databaseHostName=mysql55~g"
+expression="s~driverPath.*~driverPath=/usr/bin/chromedriver~g; s~pathToInstaller.*~pathToInstaller=http://php56/~g; s~databasePassword.*~databasePassword=root~g; s~databaseHostName.*~databaseHostName=mysql55~g; s~whindowMaximize.*~whindowMaximize=false~g"
 echo "${expression}"
 sed "${expression}" configs/Configuration.properties > Configuration.properties
 cat Configuration.properties
