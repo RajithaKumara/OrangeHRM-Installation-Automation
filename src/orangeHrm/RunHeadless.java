dde66014594d99956ca86217977f7dbaf52c0985
RajithaKumara <tmrkumara.15@cse.mrt.ac.lk>
2018-10-25 01:59:13 +0530

    Update headless driver settings

--- src/orangeHrm/RunHeadless.java
+++ src/orangeHrm/RunHeadless.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import managers.FileReaderManager;
 import pageObjects.Installer;
@@ -33,8 +33,11 @@ public class RunHeadless {
 			//Trigger UI automation
 			
 			System.setProperty("webdriver.chrome.driver", FileReaderManager.getInstance().getConfigFileReader().getDriverPath());
+
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless","--ignore-certificate-errors");  
 			
-			WebDriver driver = new ChromeDriver(); 
+			WebDriver driver = new ChromeDriver(options); 
 			
 			Installer newInstaller = new Installer(driver);
 			
