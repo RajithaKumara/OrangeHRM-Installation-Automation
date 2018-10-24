cefc5001e3d067edef3e417c8cd79328fc2ef363
RajithaKumara <tmrkumara.15@cse.mrt.ac.lk>
2018-10-25 01:19:14 +0530

    Log the generated files

--- build/travis/before_script.sh
+++ build/travis/before_script.sh
@@ -9,15 +9,19 @@ cd $ENV_DIR
 
 expression="s~LOCAL_SRC.*~LOCAL_SRC=$TRAVIS_BUILD_DIR/$APP_DIR~g"
 sed "${expression}" .env-dist > .env
+cat .env
 
 docker-compose up -d
+docker ps
 
 sleep 10
 cd ..
 
 echo "127.0.0.1    php56 php70 php71 php72" | sudo tee -a /etc/hosts
+cat /etc/hosts
 
 # Edit Configuration.properties
 expression="s~driverPath.*~driverPath=/usr/bin/chromedriver~g; s~pathToInstaller.*~pathToInstaller=http://php56/~g; s~databasePassword.*~databasePassword=root~g; s~databaseHostName.*~databaseHostName=mysql55~g; s~databasePort.*~databasePort=3324~g;"
 sed "${expression}" configs/Configuration.properties > Configuration.properties
+cat Configuration.properties
 mv Configuration.properties configs
