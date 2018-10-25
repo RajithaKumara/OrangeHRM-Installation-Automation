5a39ae1e621f11126172de56d44ece64fd43ce3b
RajithaKumara <tmrkumara.15@cse.mrt.ac.lk>
2018-10-25 23:03:34 +0530

    Add composer.phar

--- build/travis/before_script.sh
+++ build/travis/before_script.sh
@@ -5,6 +5,11 @@ git clone --single-branch -b $GIT_APP_BRANCH --depth $GIT_DEPTH $GIT_APP_URL $AP
 git clone --single-branch -b $GIT_ENV_BRANCH --depth $GIT_ENV_DEPTH $GIT_ENV_URL $ENV_DIR
 
 sudo chmod 777 -R .
+
+# Copy composer.phar to project directory
+cp composer.phar $APP_DIR
+sudo chmod 777 $APP_DIR/composer.phar
+
 cd $ENV_DIR
 
 expression="s~LOCAL_SRC.*~LOCAL_SRC=$TRAVIS_BUILD_DIR/$APP_DIR~g"
@@ -14,8 +19,9 @@ cat .env
 
 docker-compose up -d
 docker ps
-docker exec -it ohrmosdevenvironment_php-7.1_1 sh -c "composer install -d symfony/lib"
-docker exec -it ohrmosdevenvironment_php-7.1_1 sh -c "composer dump-autoload -o -d symfony/lib"
+
+docker exec -it ohrmosdevenvironment_php-7.1_1 sh -c "php composer.phar install -d symfony/lib"
+docker exec -it ohrmosdevenvironment_php-7.1_1 sh -c "php composer.phar dump-autoload -o -d symfony/lib"
 docker exec -it ohrmosdevenvironment_php-7.1_1 sh -c "cd symfony && php symfony orangehrm:publish-assets"
 docker exec -it ohrmosdevenvironment_php-7.1_1 sh -c "cd symfony && php symfony cc"
 
