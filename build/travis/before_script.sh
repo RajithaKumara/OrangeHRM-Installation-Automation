527132beef4026ccd340ac736b9fede501b9615a
rajithakumara <tmrkumara.15@cse.mrt.ac.lk>
2018-10-25 18:52:37 +0530

    Change php container

--- build/travis/before_script.sh
+++ build/travis/before_script.sh
@@ -14,15 +14,16 @@ cat .env
 
 docker-compose up -d
 docker ps
+docker exec -it ohrmosdevenvironment_php-7.1_1 sh -c "cd symfony && php symfony cc"
 
 sleep 10
 cd ..
 
-echo -e "\n127.0.0.1    php56 php70 php71 php72" | sudo tee -a /etc/hosts
+echo -e "\n127.0.0.1 php56 php70 php71 php72" | sudo tee -a /etc/hosts
 cat /etc/hosts
 
 # Edit Configuration.properties
-expression="s~driverPath.*~driverPath=/usr/bin/chromedriver~g; s~pathToInstaller.*~pathToInstaller=http://php56/~g; s~databasePassword.*~databasePassword=root~g; s~databaseHostName.*~databaseHostName=mysql55~g; s~whindowMaximize.*~whindowMaximize=false~g"
+expression="s~driverPath.*~driverPath=/usr/bin/chromedriver~g; s~pathToInstaller.*~pathToInstaller=http://php71/~g; s~databasePassword.*~databasePassword=root~g; s~databaseHostName.*~databaseHostName=mysql55~g; s~whindowMaximize.*~whindowMaximize=false~g"
 echo "${expression}"
 sed "${expression}" configs/Configuration.properties > Configuration.properties
 cat Configuration.properties
