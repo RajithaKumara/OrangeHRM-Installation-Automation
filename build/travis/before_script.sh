c941691462b980af1b2d779b1dbd424194664b13
Rajitha Kumara <tmrkumara.15@cse.mrt.ac.lk>
2018-10-26 23:50:13 +0530

    Merge develop into master (#1)
    * Change php container
    
    * Install composer dependencies
    
    * Add composer.phar
    
    * Update .travis.yml

--- build/travis/before_script.sh
+++ build/travis/before_script.sh
@@ -5,6 +5,11 @@ git clone --single-branch -b $GIT_APP_BRANCH --depth $GIT_DEPTH $GIT_APP_URL $AP
 git clone --single-branch -b $GIT_ENV_BRANCH --depth $GIT_ENV_DEPTH $GIT_ENV_URL $ENV_DIR
 
 sudo chmod 777 -R .
+
+# Copy composer.phar to project directory
+cp composer.phar $APP_DIR
+sudo chmod 777 $APP_DIR/composer.phar
+
 cd $ENV_DIR
 
 expression="s~LOCAL_SRC.*~LOCAL_SRC=$TRAVIS_BUILD_DIR/$APP_DIR~g"
@@ -15,14 +20,19 @@ cat .env
 docker-compose up -d
 docker ps
 
+docker exec -it ohrmosdevenvironment_php-7.1_1 sh -c "php composer.phar install -d symfony/lib"
+docker exec -it ohrmosdevenvironment_php-7.1_1 sh -c "php composer.phar dump-autoload -o -d symfony/lib"
+docker exec -it ohrmosdevenvironment_php-7.1_1 sh -c "cd symfony && php symfony orangehrm:publish-assets"
+docker exec -it ohrmosdevenvironment_php-7.1_1 sh -c "cd symfony && php symfony cc"
+
 sleep 10
 cd ..
 
-echo -e "\n127.0.0.1    php56 php70 php71 php72" | sudo tee -a /etc/hosts
+echo -e "\n127.0.0.1 php56 php70 php71 php72" | sudo tee -a /etc/hosts
 cat /etc/hosts
 
 # Edit Configuration.properties
-expression="s~driverPath.*~driverPath=/usr/bin/chromedriver~g; s~pathToInstaller.*~pathToInstaller=http://php56/~g; s~databasePassword.*~databasePassword=root~g; s~databaseHostName.*~databaseHostName=mysql55~g; s~whindowMaximize.*~whindowMaximize=false~g"
+expression="s~driverPath.*~driverPath=/usr/bin/chromedriver~g; s~pathToInstaller.*~pathToInstaller=http://php71/~g; s~databasePassword.*~databasePassword=root~g; s~databaseHostName.*~databaseHostName=mysql55~g; s~whindowMaximize.*~whindowMaximize=false~g"
 echo "${expression}"
 sed "${expression}" configs/Configuration.properties > Configuration.properties
 cat Configuration.properties
