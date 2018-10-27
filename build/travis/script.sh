a52a13c045bea3941ac4835f66af05406dbf5534
Rajitha Kumara <tmrkumara.15@cse.mrt.ac.lk>
2018-10-27 09:15:31 +0530

    Update script.sh

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -3,5 +3,5 @@
 # mvn exec:java -D"exec.mainClass"="orangeHrm.RunHeadless" -X
 mvn exec:java -D"exec.mainClass"="orangeHrm.RunHeadless"
 
-mkdir build/dist/$TRAVIS_JOB_NUMBER
-cp screenshot_orangehrm.png build/dist/$TRAVIS_JOB_NUMBER
+mkdir -p build/dist/travis/$TRAVIS_JOB_NUMBER
+cp screenshot_orangehrm.png build/dist/travis/$TRAVIS_JOB_NUMBER
