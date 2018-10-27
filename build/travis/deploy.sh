0876017c63e5b08bf7c48bd8d1c4a98689ed4e0e
Rajitha Kumara <tmrkumara.15@cse.mrt.ac.lk>
2018-10-27 16:30:03 +0530

    remove mkdir

--- build/travis/deploy.sh
+++ build/travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pathToArtifacts=build/dist/travis/$TRAVIS_BUILD_NUMBER/$TRAVIS_JOB_NUMBER
-mkdir -p $pathToArtifacts
+#mkdir -p $pathToArtifacts
 cp screenshot_orangehrm.png $pathToArtifacts
 echo "$TRAVIS_JOB_WEB_URL" | tee $pathToArtifacts/buildResults.txt
