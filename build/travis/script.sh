7320ef3dd87c6ccd8f433d7f6f18978303945c38
Rajitha Kumara <tmrkumara.15@cse.mrt.ac.lk>
2018-10-27 16:29:03 +0530

    Update script.sh

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# mvn exec:java -D"exec.mainClass"="orangeHrm.RunHeadless" -X
-mvn exec:java -D"exec.mainClass"="orangeHrm.RunHeadless"
-
 pathToArtifacts=build/dist/travis/$TRAVIS_BUILD_NUMBER/$TRAVIS_JOB_NUMBER
 mkdir -p $pathToArtifacts
-cp screenshot_orangehrm.png $pathToArtifacts
-echo "$TRAVIS_JOB_WEB_URL" | tee $pathToArtifacts/buildResults.txt
+
+# mvn exec:java -D"exec.mainClass"="orangeHrm.RunHeadless" -X
+mvn exec:java -D"exec.mainClass"="orangeHrm.RunHeadless" >>  $pathToArtifacts/buildLog.txt
