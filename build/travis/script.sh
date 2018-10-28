43e2d5fa920d62111500b7e736662215fe0bbe5e
RajithaKumara <tmrkumara.15@cse.mrt.ac.lk>
2018-10-29 00:06:57 +0530

    Improve directory creation for each db

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-mkdir -p $PATH_TO_ARTIFACTS
-
-# mvn exec:java -D"exec.mainClass"="orangeHrm.RunHeadless" -X
-# mvn exec:java -D"exec.mainClass"="orangeHrm.RunHeadless" >>  $PATH_TO_ARTIFACTS/buildLog.txt
-
 # `Internal Field Separator` need to split $MYSQL_CONTAINERS string into array
 IFS='|';set -f
 
@@ -14,10 +9,16 @@ echo "+--------------------------------------------+"
 echo "  Test started on $dbHost MySQL container     "
 echo "+--------------------------------------------+"
 
+mkdir -p $PATH_TO_ARTIFACTS/$dbHost
 sed -i "s~databaseHostName.*~databaseHostName=$dbHost~g" $PATH_TO_CONFIG
+
+# Run headless testing
 mvn exec:java -D"exec.mainClass"="orangeHrm.RunHeadless" >>  $PATH_TO_ARTIFACTS/$dbHost/buildLog.txt
+
+# Copy test artifacts
 cp screenshot_orangehrm.png $PATH_TO_ARTIFACTS/$dbHost
 
+# Reset installation
 cmd1="cd symfony && php symfony orangehrm:publish-assets && php symfony cc"
 cmd2="cd ../devTools/general && php reset-installation.php"
 docker exec -it os_dev_$PHP_CONTAINER sh -c "${cmd1} && ${cmd2}"
